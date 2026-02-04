7cb5e6393c643fc982828e3d4959a0d29bae0cd8
Prathmesh <work.prats2002@example.com>
2026-02-04 18:01:02 +0530

    ci pipeline and outputs file changed to previous

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,7 @@ output "alb_dns_name" {
   description = "Public DNS name of the Application Load Balancer"
   value       = aws_lb.app_alb.dns_name
 }
+output "backend_ec2_public_ip" {
+  description = "Public IP of backend EC2 instance"
+  value       = aws_instance.backend.public_ip
+}
