15efed59450a9ab0a9eda8d3ebee917542b1000e
Prathmesh <work.prats2002@example.com>
2026-02-07 17:11:18 +0530

    changed node files

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
+require("dotenv").config();   // MUST be first
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node.js CI/CD Assignment");
-});
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error:", err));
+
+app.use("/api/notes", require("./routes/notes"));
+
+app.listen(5000, () => {
+  console.log("Server running on port 3000");
 });
